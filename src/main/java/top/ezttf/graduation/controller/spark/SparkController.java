e3e31ef7a7a252dea43b4f7cd7962328e47b8ec7
yuwen <yuwen2424@163.com>
2019-03-18 16:49:00 +0800

    看看能不能做个本地侧测试

---
+++ src/main/java/top/ezttf/graduation/controller/spark/SparkController.java
@@ -0,0 +1,42 @@
+package top.ezttf.graduation.controller.spark;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.SparkContext;
+import org.apache.spark.sql.SparkSession;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import scala.Tuple2;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Pattern;
+
+/**
+ * @author yuwen
+ * @date 2019/3/18
+ */
+@RestController
+public class SparkController {
+
+    @GetMapping("wordCount")
+    public String wordCount() throws IOException {
+        BufferedWriter writer = Files.newBufferedWriter(Paths.get("~/result.txt"));
+        SparkSession spark = new SparkSession(new SparkContext(
+                new SparkConf().setMaster("local[2]").setJars(new String[]{"target/graduation-1.0.jar"})
+                        .set("spark.executor.memory", "512m")
+                        .setAppName("wordCount")
+        ));
+        spark.read()
+                .textFile("hdfs://hadoop:8020/README.txt").javaRDD()
+                .flatMap(word -> Arrays.asList(Pattern.compile(" ").split(word)).iterator())
+                .mapToPair(word -> new Tuple2<>(word, 1))
+                .reduceByKey((i, j) -> i + j)
+                .collect()
+                .forEach(out -> writer.write(out._1() + ": " + out._2()));
+        spark.stop();
+    }
+}

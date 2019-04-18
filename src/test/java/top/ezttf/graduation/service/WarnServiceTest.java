dc9a1455cfc5d88edd8a4a231268decd0488f5c8
yuwen <yuwen2424@163.com>
2019-04-18 16:35:16 +0800

    4/18

--- src/test/java/top/ezttf/graduation/service/WarnServiceTest.java
+++ src/test/java/top/ezttf/graduation/service/WarnServiceTest.java
@@ -1,6 +1,5 @@
 package top.ezttf.graduation.service;
 
-import com.alibaba.fastjson.JSON;
 import com.spring4all.spring.boot.starter.hbase.api.HbaseTemplate;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.time.DateUtils;
@@ -17,10 +16,12 @@ import org.springframework.test.context.junit4.SpringRunner;
 import top.ezttf.graduation.constant.Constants;
 import top.ezttf.graduation.mapper.WarnRowMapper;
 import top.ezttf.graduation.vo.ReceiveData;
+import top.ezttf.graduation.vo.UserInfo;
 import top.ezttf.graduation.vo.Warn;
 
 import java.util.Date;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * @author yuwen
@@ -41,21 +42,36 @@ public class WarnServiceTest {
     public void testSaveOrUpdates() {
 
         log.debug("{}", new Date());
+        for (int i = 0; i < 40; i++) {
+            int listSize = ThreadLocalRandom.current().nextInt(0, 40);
+            List<UserInfo> list = Lists.newArrayList();
+            for (int j = 0; j < listSize; j++) {
+                int element = ThreadLocalRandom.current().nextInt(20);
+                UserInfo userInfo = new UserInfo();
+                userInfo.setCh(element);
+                list.add(userInfo);
+                ReceiveData receiveData = new ReceiveData("id", list,
+                        "mmac", 2,
+                        DateUtils.addDays(new Date(), 1), "lat", "lon");
+                Warn warn = Warn.assembleFromReceiveData(receiveData);
+                iWarnService.saveWarn(warn);
+            }
+        }
 
-        ReceiveData receiveData = new ReceiveData("id", Lists.newArrayList(),
-                "mmac", 2,
-                DateUtils.addDays(new Date(), 1), "lat", "lon");
-        Warn warn = Warn.assembleFromReceiveData(receiveData);
-        iWarnService.saveWarn(warn);
-
-        receiveData = new ReceiveData("idid", Lists.newArrayList(),
-                "mmac", 2,
-                DateUtils.addDays(new Date(), 2), "latlat", "lonlon");
-        iWarnService.saveWarn(Warn.assembleFromReceiveData(receiveData));
-
-        Scan scan = new Scan();
-        List<Warn> warnList = hbaseTemplate.find(Constants.WarnTable.TABLE_NAME, scan, new WarnRowMapper());
-        warnList.forEach(warnE -> log.debug(JSON.toJSONString(warnE)));
+//        ReceiveData receiveData = new ReceiveData("id", Lists.newArrayList(),
+//                "mmac", 2,
+//                DateUtils.addDays(new Date(), 1), "lat", "lon");
+//        Warn warn = Warn.assembleFromReceiveData(receiveData);
+//        iWarnService.saveWarn(warn);
+//
+//        receiveData = new ReceiveData("idid", Lists.newArrayList(),
+//                "mmac", 2,
+//                DateUtils.addDays(new Date(), 2), "latlat", "lonlon");
+//        iWarnService.saveWarn(Warn.assembleFromReceiveData(receiveData));
+//
+//        Scan scan = new Scan();
+//        List<Warn> warnList = hbaseTemplate.find(Constants.WarnTable.TABLE_NAME, scan, new WarnRowMapper());
+//        warnList.forEach(warnE -> log.debug(JSON.toJSONString(warnE)));
     }
 
     public void testSpark() throws InterruptedException {
@@ -65,5 +81,11 @@ public class WarnServiceTest {
 
         streamingContext.start();
         streamingContext.awaitTermination();
+    }
+
+    @Test
+    public void readHbase() {
+        List<Warn> warns = hbaseTemplate.find("graduation:warn", new Scan(), new WarnRowMapper());
+        warns.forEach(warn -> log.debug("{}", warn.getCount()));
     }
 }

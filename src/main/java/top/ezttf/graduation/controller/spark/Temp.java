c74b5ea3162d5cbad55c6085d1f249a0fb479b5f
yuwen <yuwen2424@163.com>
2019-04-23 15:06:08 +0800

    temp

--- src/main/java/top/ezttf/graduation/controller/spark/Temp.java
+++ src/main/java/top/ezttf/graduation/controller/spark/Temp.java
@@ -19,12 +19,4 @@ public class Temp implements Serializable {
     private double count;
     private double random;
 
-
-    private double features;
-
-    public Temp(double time, double count, double random) {
-        this.time = time;
-        this.count = count;
-        this.random = random;
-    }
 }
